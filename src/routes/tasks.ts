3f377ce051d6f80a8c9e6a44788642668476b848
Artem Gladkov <mamifero3004@gmail.com>
2023-06-09 02:03:09 +0200

    Update a task

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express'
 import { Task } from '../models/task'
+import { request } from 'http'
+import { parse } from 'path'
 
 const router = Router()
 let tasks: Task[] = []
@@ -24,6 +26,18 @@ router.get('/', (req: Request, res: Response) => {
     if (!task) {
         res.status(404).send('Task not found')
     } else {
+        res.json(task)
+    }
+})
+
+router.put('/', (req: Request, res: Response) => {
+    const task = tasks.find(t => t.id === parseInt(req.params.id))
+    if (!task) {
+        res.status(404).send('Task not found')
+    } else {
+        task.title = req.body.title || task.title
+        task.description = req.body.description || task.description
+        task.completed = req.body.completed || task.completed
         res.json(task)
     }
 })
